bdf19590fb8094435725072bf86eac6f5e99d9bb
Anna <sviridova_anya@mail.ru>
2022-12-24 23:06:06 +0300

    Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+int EnterNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int M = EnterNumber("Введите натуральное число M");
+int N = EnterNumber("Введите натуральное число N");
+
+int SumNumbers(int min, int max)
+{
+    if (min == max)
+    {
+        return min;
+    }
+    else 
+    {
+        return SumNumbers(min, max - 1) + max;
+    }
+}
+Console.WriteLine(SumNumbers(M, N));
